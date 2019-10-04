c57118f1a50e912306dffa3774e999d650df8bab
tojuofe <tojuofe@gmail.com>
2019-10-04 19:34:16 +0100

    commit again

--- server.js
+++ server.js
@@ -1,29 +1,16 @@
 const express = require('express');
 const app = express();
-const ConnectDB = require('./config/db');
-
-// Connect DB
-ConnectDB();
 
 // init middleware
 app.use(express.json({ extended: false }));
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Started on Port ${PORT}`));
 
 // Create Route
-app.get('/', (req, res) => res.json({ msg: 'Welcome from CryptoBit...' }));
+app.get('/', (req, res) => res.json({ msg: 'Welcome from Online Store' }));
 
 // Define Routes
-app.use('/api/admin', require('./routes/Admin'));
-app.use('/api/authadmin', require('./routes/authAdmin'));
-app.use('/api/walletno', require('./routes/WalletNo'));
-app.use('/api/user', require('./routes/User'));
-app.use('/api/auth', require('./routes/Auth'));
-app.use('/api/ticket', require('./routes/Ticket'));
-app.use('/api/deposit', require('./routes/Deposit'));
-app.use('/api/withdraw', require('./routes/Withdraw'));
-app.use('/api/profile', require('./routes/Profile'));
-app.use('/api/forgotpassword', require('./routes/ForgotPassword'));
-app.use('/api/contact', require('./routes/Contact'));
+app.use('/api/downloadpdf', require('./routes/DownloadPdf'));
+
